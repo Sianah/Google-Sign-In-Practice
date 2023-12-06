446c04d05ccee36218913b147c573ba342ee1258
Sisi Small <smallsianah@gmail.com>
2023-12-05 23:07:03 -0500

    added notes

--- tutorial-google/App.js
+++ tutorial-google/App.js
@@ -1,4 +1,6 @@
 //web client ID - 508324923582-colqt82dke17aa8n8n51msqric08j9sm.apps.googleusercontent.com
+//ios client ID - 508324923582-qtv5h7tf8eub87iett2ek12hu6nflpv2.apps.googleusercontent.com
+//android client ID - 508324923582-ncuan2li0la71d0oe8c317lg7l3d8g6d.apps.googleusercontent.com
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
